bd053edcd258098f546ce0ef83a2ea65ce75ccf0
Winktabulous <mike.winkler90@gmail.com>
2023-02-05 16:42:26 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -8,16 +8,15 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Toolbox</title>
+        <title>Big-Cranium Time</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Next.js Toolbox" />
+        <Header title="Sanium Craniums" />
         <hr />
         <p className="description">
-          Here's an example of a Netlify Form! When you fill this out, the
-          submissions can be found in the Netlify Admin site.
+          Form test, should send email to mike.winkler90@gmail.com.
         </p>
         <FeedbackForm />
         <JokeBlock />
